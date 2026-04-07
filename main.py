04b6b25f6b8396ef58fed0bff0168a12404601c6
Haziel0303 <142752439+Haziel0303@users.noreply.github.com>
2026-04-07 11:38:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.routes import database, tally
+from fastapi import Request
+from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 
@@ -7,5 +9,8 @@ app.include_router(tally.router)
 app.include_router(database.router)
 
 @app.get("/")
-async def root():
+async def root(request: Request):
+    echostr = request.query_params.get("echostr")
+    if echostr:
+        return PlainTextResponse(echostr)
     return {"message": "server is working"}
